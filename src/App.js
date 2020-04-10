f15871b8e6ee5af56d5c04abf999040885cd5e39
Pau Bonilla <paulo111995@gmail.com>
2020-04-09 18:32:53 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.css';
 
+import Game from './components/Game';
+
 function App() {
   return (
     <div className="App">
-      tic tac toe
+      <Game />
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Board from './Board';
+
+export default class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            xIsNext: true,
+            stepNumber: 0,
+            history: [
+                { squares: Array(9).fill(null) }
+            ]
+        }
+    }
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat({
+                squares: squares
+            }),
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
+        })
+
+    }
+    render() {
+        const history = this.state.history
+        const current = history[this.state.stepNumber];
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <div className="title">tic tac toe</div>
+                    <Board 
+                        onClick={(i) => this.handleClick(i)}
+                        squares={current.squares}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
