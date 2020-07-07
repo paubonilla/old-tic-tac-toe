0597fbe890a2361db7eb8bf8fe4eb5c8f421c5b6
Pau Bonilla <paulo111995@gmail.com>
2020-07-06 23:02:31 -0700

    added build folder; node_modules

---
+++ build/precache-manifest.33db93a57eedae0b1509e13d681085a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497ba5c84f6e418cf93d0b77987c8bc4",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d40ba18f9495afcadc98",
+    "url": "/tic-tac-toe/static/css/main.8976c7aa.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/tic-tac-toe/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40ba18f9495afcadc98",
+    "url": "/tic-tac-toe/static/js/main.3db2e54a.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
