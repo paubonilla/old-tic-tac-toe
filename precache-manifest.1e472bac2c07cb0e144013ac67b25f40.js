b4f2adc06a1cac673b56830cdaa37676ac4d0195
Pau Bonilla <paulo111995@gmail.com>
2020-07-07 15:25:30 -0700

    Updates

---
+++ precache-manifest.1e472bac2c07cb0e144013ac67b25f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3bfa0ad247be583e33824a59c83631",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5ff0169179cf175fc575",
+    "url": "/tic-tac-toe/static/css/main.70f1f089.chunk.css"
+  },
+  {
+    "revision": "09f9296f6b3b07cba23f",
+    "url": "/tic-tac-toe/static/js/2.288cea6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.288cea6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff0169179cf175fc575",
+    "url": "/tic-tac-toe/static/js/main.64e6cb7e.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
