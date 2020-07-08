a3fc461a71675c78938d47348546a5aa0dcddedc
Pau Bonilla <paulo111995@gmail.com>
2020-07-08 02:05:01 -0700

    initial commit

---
+++ build/precache-manifest.bc2aa210f9f7d5c8d750d41eb37aa220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03e500d102245f7215b030d38d0371d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "c24de83860cb00ac3516",
+    "url": "/tic-tac-toe/static/css/main.702f1548.chunk.css"
+  },
+  {
+    "revision": "6940c66425064c725afc",
+    "url": "/tic-tac-toe/static/js/2.86eb3687.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.86eb3687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24de83860cb00ac3516",
+    "url": "/tic-tac-toe/static/js/main.2dd92899.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  },
+  {
+    "revision": "b09e93bf54cc2d253f6edebbfbc6655b",
+    "url": "/tic-tac-toe/static/media/michael.b09e93bf.png"
+  }
+]);
